e4cc33c861358bc215dec9a378671a9ab6ec63af
MinulGamage <113190129+MinulGamage@users.noreply.github.com>
2022-12-06 10:16:31 -0500

    vite

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,6 +1,9 @@
 // import "../styles/style.css";
 import "../styles/style.css";
 import { setupCounter } from "./counter.js";
+import AOS from "aos";
+
+AOS.init();
 import { menu } from "./menu";
 
 document.querySelector("#app").innerHTML = `
