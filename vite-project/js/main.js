161994030778bc50c888b99ebef7d9d26eca4a77
MinulGamage <113190129+MinulGamage@users.noreply.github.com>
2022-12-06 10:03:45 -0500

    vite 3

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,24 +1,10 @@
 // import "../styles/style.css";
 import "../styles/style.css";
-import javascriptLogo from "./javascript.svg";
 import { setupCounter } from "./counter.js";
+import { menu } from "./menu";
 
 document.querySelector("#app").innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite?</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
+<h1>Hellortryeyryyeryeyey</h1>
 `;
 
-setupCounter(document.querySelector("#counter"));
+console.log(menu);
