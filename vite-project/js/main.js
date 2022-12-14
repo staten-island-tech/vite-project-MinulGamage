e0181014f22c09a8254309b95754f1f55c447fb8
MinulGamage <113190129+MinulGamage@users.noreply.github.com>
2022-12-14 10:15:52 -0500

    vite 8

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,11 +1,26 @@
 import "../styles/style.css";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import { DOMSelectors } from "./DOM";
 
 AOS.init();
 import { menu } from "./menu";
 
-document.querySelector("#app").innerHTML = `
-`;
+document.querySelector("#app").innerHTML = `<div class="parent">
+<p>${menu.name}</p>
+<img src="${menu.img}"/>
+<p class="PricePerPie">Price Per Pie:${menu.PricePerPie}</p>
+<p class="PricePerSlice">Price Per Slice:${menu.PricePerSlice}</p>
+</div>`;
 
 console.log(menu);
+
+DOMSelectors.button.addEventListener("click", function () {
+  if (document.body.classList.contains("lightmode")) {
+    document.body.classList.add("darkmode");
+    document.body.classList.remove("lightmode");
+  } else {
+    document.body.classList.add("lightmode");
+    document.body.classList.remove("darkmode");
+  }
+});
