a093a747935c582b96294231de993faf6bddefda
MinulGamage <113190129+MinulGamage@users.noreply.github.com>
2022-12-19 10:16:10 -0500

    vite 11

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -6,13 +6,38 @@ import { DOMSelectors } from "./DOM";
 AOS.init();
 import { menu } from "./menu";
 
-document.querySelector("#app").innerHTML = `<div class="card">
+/* document.getElementById("#app").insertAdjacentHTML(
+  "afterend,"`<div class="card">
+<h3>${menu.name}</h3>
+<img src="${menu.img}"/>
+<p class="PricePerPie">Price Per Pie: ${menu.PricePerPie}</p>
+<p class="PricePerSlice">Price Per Slice: ${menu.PricePerSlice}</p>
+</div>`
+); */
+
+document.querySelector("#app").innerHTML = `
+<div class="card">
 <h3>${menu.name}</h3>
 <img src="${menu.img}"/>
 <p class="PricePerPie">Price Per Pie: ${menu.PricePerPie}</p>
 <p class="PricePerSlice">Price Per Slice: ${menu.PricePerSlice}</p>
 </div>`;
 
+/* menu.forEach((menu) => {
+  DOMSelectors.displayContainer.insertAdjacentHTML(
+    "afterbegin",
+    `<div class="card">
+   <h2 class="item">${menu.name}</h2>
+   <div class="img-container">
+   <img src="${menu.img}"
+   alt="" class="menu-img">
+   </div>
+   <h3 class="PricePerPie"> ${menu.PricePerPie}</h3>
+   <h3 class="PricePerSlice">${menu.PricePerSlice}</h3>
+   </div>`
+  );
+});
+ */
 console.log(menu);
 
 DOMSelectors.button.addEventListener("click", function () {
