a5636ca1d8450dd1d65a6c94b6e8c2fde4fc863e
MinulGamage <113190129+MinulGamage@users.noreply.github.com>
2022-12-21 10:16:50 -0500

    vite 13

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -27,14 +27,14 @@ menu.forEach((menu) => {
   DOMSelectors.box.insertAdjacentHTML(
     "afterbegin",
     `
-    <div class="card">
+    <div class="menu-card">
    <h2 class="item">${menu.name}</h2>
    <div class="img-container">
    <img src="${menu.img}"
    alt="" class="menu-img">
    </div>
-   <h3 class="PricePerPie"> ${menu.PricePerPie}</h3>
-   <h3 class="PricePerSlice">${menu.PricePerSlice}</h3>
+   <h3 class="PricePerPie"> Price Per Pie: $${menu.PricePerPie}</h3>
+   <h3 class="PricePerSlice"> Price Per Slice: $${menu.PricePerSlice}</h3>
    </div>`
   );
 });
